119f2e55d5280338235c2672ce92c2983694b1b6
brian <barazzabryho24@gmail.com>
2022-03-18 12:20:17 +0300

    enhance trainer dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,7 @@
 <?php  
     session_start();
+    require('db/dbconfig.php');
+    require('db/dept.php');
     if (!isset($_SESSION['login'])){ 
         header('location:login.php');
     }
@@ -13,123 +15,79 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Dashboard</li>
-                        </ol>
+                        <h1 class="mt-4">Trainer Dashboard</h1>
+                        <?php
+                            $empno = $_SESSION['empno'];
+                            $query = "SELECT `name` FROM trainer WHERE `emp_no` = '$empno'";
+                            $ret = mysqli_query($con, $query);                            
+                            while ($row = mysqli_fetch_array($ret)) {                                                                                            
+                            ?>
+                            <ol class="breadcrumb mb-4">
+                                <li class="breadcrumb-item active"><?php echo $row['name'];?></li>
+                            </ol>
+                        <?php }?>
+                       
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Uploaded Content</div>
+                                    <div class="card-body">Upload Content</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="newtask.php">upload new content</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Completed Content</div>
+                                <div class="card bg-secondary text-white mb-4">
+                                    <div class="card-body">Submissions</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="#">View Submissions</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Pending</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Cancelled</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
+                            </div>                        
                         </div>        
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                In Progress Tasks
+                                All Tasks
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple" class="table table-striped table-hover table-bordered">
                                     <thead>
                                         <tr>
-                                            <th>Content type</th>
+                                            <th>#</th>
+                                            <th>Class</th>
+                                            <th>Type</th> 
+                                            <th>Urgency</th>
                                             <th>Description</th>
-                                            <th>Created at</th>
-                                            <th>Submission</th>
-                                            <th>In progress?</th>
-                                            <th>Action</th>
+                                            <th>Status</th>
+                                            <th>Action</th>                                           
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Discussion </td>
-                                            <td>Discussion</td>
-                                            <td>2011/04/25</td>
-                                            <td>-</td>
-                                            <td>yes</td>
-                                            <td>------</td>
-                                        </tr>
+                                        <?php
+                                            $empno = $_SESSION['empno'];
+                                            $query = "SELECT * FROM task WHERE `trainer` = '$empno'";
+                                            $ret = mysqli_query($con, $query);
+                                            $cnt = 1;
+                                            while ($row = mysqli_fetch_array($ret)) {                                                                                            
+                                        ?>
                                         
+                                        <tr>
+                                            <td><?php echo $cnt;?></td>
+                                            <td><?php echo $row['class'];?></td>
+                                            <td><?php echo $row['type'];?></td>
+                                            <td><?php echo $row['urgency'];?></td>
+                                            <td><?php echo $row['descr'];?></td>
+                                            <td><?php echo $row['status'];?></td>
+                                            <!-- <td><i class="fa fa-eye" ></i> view</td> -->
+                                            <td>                                                
+                                                <a href="edittrainee.php?updatereg=<?php echo $row['reg_no'];?>"><button type="submit" class="btn btn-sm btn-warning"> <i class="fa fa-edit" ></i> update</button></a>
+                                                <a href="edittrainee.php?updatereg=<?php echo $row['reg_no'];?>"><button type="submit" class="btn btn-sm btn-secondary"> <i class="fa fa-eye" ></i> view</button></a>
+                                            </td>
+                                        </tr>
+                                        <?php $cnt=$cnt+1;}?>                                        
                                     </tbody>
                                 </table>
                             </div>
