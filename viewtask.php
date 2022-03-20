a93b2991a7607ee7c2209d02d29d8e580a739588
brian <barazzabryho24@gmail.com>
2022-03-20 17:19:57 +0300

    view task page

--- viewtask.php
+++ viewtask.php
@@ -1,10 +1,16 @@
-<?php  
+<?php 
     session_start();
     require('db/dbconfig.php');
-    require('db/dept.php');
+    require('db/class.php');
+    require("api/sms.php");
     if (!isset($_SESSION['login'])){ 
         header('location:login.php');
     }
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['close'])) {        
+        header("location:submissions.php");           
+}
+// $AT.sendMessage($recipients, $message);
+     
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,60 +21,53 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Trainer Dashboard</h1>
-                        <?php
-                            $empno = $_SESSION['empno'];
-                            $query = "SELECT `name` FROM trainer WHERE `emp_no` = '$empno'";
-                            $ret = mysqli_query($con, $query);                            
-                            while ($row = mysqli_fetch_array($ret)) {                                                                                            
-                            ?>
-                            <ol class="breadcrumb mb-4">
-                                <li class="breadcrumb-item active"><?php echo $row['name'];?></li>
-                            </ol>
-                        <?php }?>
-                       
-                        <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Upload Content</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="newtask.php">upload new content</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        <h2>Task Details</h2>
+                        <br>                                                                                                     
+                        <?php 
+                         $id = $_GET['id'];                                
+                         // $con = $conn;
+                         $query = "SELECT * FROM task WHERE `task`.`id` = '$id'";
+                         $ret = mysqli_query($con, $query);
+                         while ($row=mysqli_fetch_array($ret)) { ?>
+                            <div class="row">
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="card bg-secondary text-white mb-4">
+                                        <div class="card-header big">
+                                        <i class="fa fa-book"></i> <?php  echo $row['subject'];?></div>
+                                        <div class="card-body">
+                                            <h3><?php  echo $row['type'];?></h3>
+                                            <p><?php  echo $row['descr'];?></p>
+                                        </div>
+                                        <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <p class="small text-white"><strong>Status: </strong><?php  echo $row['status'];?></p>                                        
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-secondary text-white mb-4">
-                                    <div class="card-body">Submissions</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="submissions.php">View Submissions</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
                                 </div>
-                            </div>                        
-                        </div>        
+                            </div>                                                                                                                                                                                                       
+                        <?php } ?>
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                All Tasks
+                                Submitted tasks
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple" class="table table-striped table-hover table-bordered">
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>Subject</th>
+                                            <th>Student</th>
                                             <th>Type</th> 
                                             <th>Urgency</th>
                                             <th>Description</th>
-                                            <!-- <th>Status</th>                                             -->
+                                            <th>Status</th>
+                                            <th>Comments</th>
                                             <th>Action</th>                                           
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php
                                             $empno = $_SESSION['empno'];
-                                            $query = "SELECT * FROM `task` WHERE `task`.`trainer` = '$empno'";
+                                            $query = "SELECT * FROM `submission`, `task`, `student`, `trainer` WHERE `submission`.`taskid` = `task`.`id` AND `task`.`id` = '$id' AND `task`.`trainer` = '$empno' AND `task`.`trainer` = `trainer`.`emp_no` AND `submission`.`reg_no` = `student`.`reg_no`;";
                                             $ret = mysqli_query($con, $query);
                                             $cnt = 1;
                                             while ($row = mysqli_fetch_array($ret)) {                                                                                            
@@ -76,14 +75,15 @@
                                         
                                         <tr>
                                             <td><?php echo $cnt;?></td>
-                                            <td><?php echo $row['subject'];?></td>
+                                            <td><?php echo $row['reg_no'];?></td>
                                             <td><?php echo $row['type'];?></td>
                                             <td><?php echo $row['urgency'];?></td>
                                             <td><?php echo $row['descr'];?></td>
-                                            <!-- <td><?php echo $row['status'];?></td> -->
+                                            <td><?php echo $row['status'];?></td>
+                                            <td><?php echo $row['comment'];?></td>
                                             <!-- <td><i class="fa fa-eye" ></i> view</td> -->
-                                            <td>                                                                                                                                               
-                                                <a href="viewtask.php?id=<?php echo $row['id'];?>"><button type="submit" class="btn btn-sm btn-warning"> <i class="fa fa-eye" ></i> view</button></a>
+                                            <td>                                                                                                
+                                                <a href="viewsubmission.php?id=<?php echo $row['sid'];?>"><button type="submit" class="btn btn-sm btn-primary"> <i class="fa fa-eye" ></i> view</button></a>
                                             </td>
                                         </tr>
                                         <?php $cnt=$cnt+1;}?>                                        
@@ -91,6 +91,7 @@
                                 </table>
                             </div>
                         </div>
+
                     </div>
                 </main>
                 <?php include('./includes/footer.html') ?>
