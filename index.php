d17ae040d6f0e083577aa56f46370420ebc6a47a
brian <barazzabryho24@gmail.com>
2022-03-21 07:12:06 +0300

    UI: enhance homepage

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <?php include "./includes/header.html" ?>
 <body>
 <main>
-  <?php include "./includes/nav.html" ?>
+ 
   <div class="jumbotron text-center header-custom text-white" style="margin-bottom:0">
   <img src="images/logo.png" alt="" srcset="">
   <h1>Sang'alo Institute of Science and Technology</h1>
   <h3>Academic Progress Notification System</h3> 
   </div>  
-  <div class="card shadow-lg border-0 rounded-lg mt-5">
+  <div align="center"   class="card shadow-lg border-0 rounded-lg mt-5">
     <div class="card-body">                        
       <h3>WELCOME TO ACADEMIC PROGRESS NOTIFICATION MANAGEMENT INFORMATION SYSTEM</h3>
       <p class="lead">the  text below contains the instructions of how to use the system.</p>
@@ -17,34 +17,62 @@
     </div>
   </div>
   <br> 
-  <h4 align="center">SECTIONS OF THIS SYSTEM</h4>      
-  <div class="card shadow-lg border-0 rounded-lg mt-5">
-    <div class="card-body">     
-      <p><strong>1. Trainers </strong></p>.
-      <p> This are teachers who teach at the institute.They are  players when it comes to usage of this system because, they are the  who are responsible for informing trainees when they upload notes and assignment on the LMS Learning Management </p></p>                         
+  <h4 align="center">SECTIONS OF THIS SYSTEM</h4>
+  <div class="row">
+  <div class="col-xl-3 col-md-6">      
+  <div class="card bg-primary text-white mb-4">
+    <div class="card-header"> 
+    <h4>1. Trainers </h4>
+    </div>
+    <div class="card-body">       
+      <p> This are teachers who teach at the institute.They are  players when it comes to usage of this system because, they are the  who are responsible for informing trainees when they upload notes and assignment on the system </p></p>                         
+    </div>
+    <div class="card-footer d-flex align-items-center justify-content-between">
+      <a class="small text-white stretched-link" href="login.php">Login</a>
+      <div class="small text-white"><i class="fas fa-angle-right"></i></div>
     </div>
   </div>
-  <div class="card shadow-lg border-0 rounded-lg mt-5">
+</div>
+<div class="col-xl-3 col-md-6">
+  <div class="card bg-secondary text-white mb-4">
+  <div class="card-header"> 
+    <h4>2. Trainees </h4>
+    </div>
     <div class="card-body">
-      <p><strong>2. TRAINEES </strong></p>.
       <p> These are students who pursues different courses  within the Institute/College, they are responsible by ensuring that they study notes send by trainess and also carry out any assignments from the Trainers. they work  on the assignments given by respective Trainers and status of the assignments done is made available to the Respective Trainer who posted the assignment for students to carry out. </p>
     </div>
+    <div class="card-footer d-flex align-items-center justify-content-between">
+      <a class="small text-white stretched-link" href="login.php">Login</a>
+      <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+    </div>
   </div>
-  <div class="card shadow-lg border-0 rounded-lg mt-5">
+</div>
+<div class="col-xl-3 col-md-6">
+  <div class="card bg-warning text-white mb-4">
+  <div class="card-header"> 
+    <h4>3. Admin </h4>
+    </div>
     <div class="card-body">
-      <p><strong>3. Admin </strong></p>.
       <p> This is school administrator in charge of the system. He/ she is previlaged to edit, remove or add trainers and Trainees. He is in charge of creating departments, classes and subjects </p>
     </div>
-  </div>
-  <div class="card shadow-lg border-0 rounded-lg mt-5">
-    <div class="card-body">
-      <hr class="hr1"> 
-      <h2>GETTING STARTED</h2>
-      <p> TO LOGIN<li><a href="login.php" class="btn btn-light btn-radius btn-brd grd1 effect-1 butn">CLICK HERE</a></li></p>
-      <p><storng> NOTE:</p></storng>
-      <p> 1. All Trainers  and trainees will be registered by the system admin  </p>            
+    <div class="card-footer d-flex align-items-center justify-content-between">
+      <a class="small text-white stretched-link" href="login.php">Login</a>
+      <div class="small text-white"><i class="fas fa-angle-right"></i></div>
     </div>
   </div>
+  </div>
+  </div>
+  <br>
+  <div class="card mb-4">
+    <div class="card-header"> 
+      <h4>Getting started </h4>
+    </div>
+    <div class="card-body">            
+      Contact the administrator to create an account for you if you dont have one
+      <br><p><strong> NOTE:</strong>
+      <br> 1. All Trainers  and trainees will be registered by the system admin  </p>            
+    </div>
+</div>
   <?php include('./includes/footer.html') ?>  
 </main>
 </body>
